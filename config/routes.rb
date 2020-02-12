b39aa351e640b644ebba38c60380333dd4ee88ba
Colin Gilchrist <cmgilchrist98@gmail.com>
2020-02-12 12:50:11 -0500

    Catching up to current layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get 'static_pages/home'
 
+  root 'static#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
